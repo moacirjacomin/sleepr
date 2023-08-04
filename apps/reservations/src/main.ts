daa65dd0554de5b06d55fbe7531b5b516c1799d0
Moacir Jacomin <moa_x@yahoo.com.br>
2023-08-04 20:34:58 +0200

    v2
    new problems

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,9 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  console.log('FODASE....');
-
-  //
+  
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
